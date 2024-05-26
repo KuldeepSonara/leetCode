3932af8a32807f2d894746b86ab9ff7cfdf5d664
Kuldeep Sonara <93315882+KuldeepSonara@users.noreply.github.com>
2024-05-26 22:46:14 +0530

    Time: 2 ms (72.30%), Space: 44.3 MB (45.31%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -1,8 +1,9 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
         Arrays.sort(prices);
-        if(prices[0]+prices[1] <= money){
-            return money-(prices[0]+prices[1]);
+        int cost = prices[0]+prices[1];
+        if( cost<= money){
+            return money-cost;
         }else{
             return money;
         }
